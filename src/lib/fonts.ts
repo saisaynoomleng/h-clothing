1d8615e48a05d745fdc0432acebbb1709a0efc32
saisaynoomleng <saileng9723@gmail.com>
2026-04-09 19:59:02 -0400

    finish initialize app

---
+++ src/lib/fonts.ts
@@ -0,0 +1,153 @@
+import localFont from 'next/font/local';
+
+export const karla = localFont({
+  src: [
+    {
+      path: '../fonts/karla/Karla-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/karla/Karla-ExtraLightItalic.ttf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/karla/Karla-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/karla/Karla-LightItalic.ttf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/karla/Karla-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/karla/Karla-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/karla/Karla-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/karla/Karla-MediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/karla/Karla-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/karla/Karla-SemiBoldItalic.ttf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/karla/Karla-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/karla/Karla-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/karla/Karla-Extrabold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/karla/Karla-ExtraboldItalic.ttf',
+      weight: '800',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-karla',
+});
+
+export const newsreader = localFont({
+  src: [
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-ExtraLightItalic.ttf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-LightItalic.ttf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-MediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-SemiBoldItalic.ttf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/newsreader/Newsreader_14pt-ExtraBoldItalic.ttf',
+      weight: '800',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-newsreader',
+});
